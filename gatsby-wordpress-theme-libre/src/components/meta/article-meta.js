c595a6b363e9967ecfd2f5f54f2c9a32e8dec4a6
Shyam <shyamlohar1996@gmail.com>
2020-03-16 15:25:46 +0530

    added image meta tags for post and other other minot improvements

--- gatsby-wordpress-theme-libre/src/components/meta/article-meta.js
+++ gatsby-wordpress-theme-libre/src/components/meta/article-meta.js
@@ -3,6 +3,7 @@ import Helmet from "react-helmet";
 import { graphql, useStaticQuery } from "gatsby";
 import url from "url";
 import { globalHistory } from "@reach/router";
+import capitalize from "../../utils/capitalizeString";
 
 const ArticleMeta = ({ data }) => {
   const {
@@ -15,24 +16,45 @@ const ArticleMeta = ({ data }) => {
     }
   `);
 
-  console.log(url, "this is url ");
-
   const canonicalUrl = url.resolve(baseUrl, globalHistory.location.pathname);
+
+  const feature_image = data.jetpack_featured_media_url.localFile;
+
+  console.log(feature_image, "feature image is here ");
 
   return (
     <>
       <Helmet>
-        <title>{data.title}</title>
+        <title>{`${name} | ${capitalize(data.title)}`}</title>
         <meta name="description" content={data.plainExcerpt} />
         <link rel="canonical" href={canonicalUrl} />
         <meta property="og:site_name" content={name} />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content={name} />
+        <meta
+          property="og:title"
+          content={`${name} | ${capitalize(data.title)}`}
+        />
         <meta property="og:description" content={data.plainExcerpt} />
         <meta property="og:url" content={canonicalUrl} />
-        <meta name="twitter:title" content={name} />
+        {feature_image && (
+          <meta
+            property="og:image"
+            content={feature_image.childImageSharp.fluid.src}
+          />
+        )}
+        <meta
+          name="twitter:title"
+          content={`${name} | ${capitalize(data.title)}`}
+        />
         <meta name="twitter:description" content={data.plainExcerpt} />
         <meta name="twitter:url" content={canonicalUrl} />
+        {feature_image && (
+          <meta name="twitter:card" content="summary_large_image" />
+        )}
+        <meta
+          name="twitter:image"
+          content={feature_image.childImageSharp.fluid.src}
+        />
       </Helmet>
     </>
   );
---
+++ gatsby-wordpress-theme-libre/src/utils/capitalizeString.js
@@ -0,0 +1,3 @@
+export default str => {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
