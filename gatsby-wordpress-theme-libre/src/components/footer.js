6071014910ee3bdce1f5263912f166e210ae513e
Shyam <shyamlohar1996@gmail.com>
2020-04-28 13:32:55 +0530

    added rss feed link in footer and content update

--- gatsby-wordpress-theme-libre/src/components/footer.js
+++ gatsby-wordpress-theme-libre/src/components/footer.js
@@ -1,17 +1,15 @@
 import React from "react";
+import rssLogo from "../images/rss.svg";
 
 const Footer = () => {
   return (
     <div>
       <footer id="colophon" className="site-footer" role="contentinfo">
         <div className="site-info">
-          {" "}
-          <a href="http://wordpress.org/">Proudly powered by Gatsby</a>{" "}
-          <span className="sep"> | </span> Theme: Libre 2 by{" "}
-          <a href="http://wordpress.com/themes/" rel="designer">
-            Automattic & Armada
-          </a>
-          .
+          Made with DraftBox ·{" "}
+          <a href="/rss.xml" style={{ fontStyle: "normal" }}>
+            RSS <img style={{ height: "12px" }} src={rssLogo} alt="" />
+          </a>{" "}
         </div>{" "}
       </footer>
     </div>
