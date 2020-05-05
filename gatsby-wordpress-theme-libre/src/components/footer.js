ffe2c039a6859ccf0a0a741343dc8e5b6416c76e
Shyam <shyamlohar1996@gmail.com>
2020-05-05 12:50:37 +0530

    updated footer with important links and cresit link and ui changes

--- gatsby-wordpress-theme-libre/src/components/footer.js
+++ gatsby-wordpress-theme-libre/src/components/footer.js
@@ -1,17 +1,48 @@
 import React from "react";
 import rssLogo from "../images/rss.svg";
+import { Link, useStaticQuery } from "gatsby";
 
 const Footer = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      wordpressSiteMetadata {
+        name
+      }
+    }
+  `);
   return (
-    <div>
+    <div className="footer-container">
       <footer id="colophon" className="site-footer" role="contentinfo">
-        <div className="site-info">
+        <div className="copyright">
+          {data.wordpressSiteMetadata.name} © {new Date().getFullYear()}
+        </div>
+
+        <ul className="footer-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <a href="/sitemap.xml">Sitemap</a>
+          </li>
+          <li>
+            <a href="/rss.xml">RSS</a>
+          </li>
+          <li>
+            <Link to="/contact">Contact Us</Link>
+          </li>
+        </ul>
+
+        {/* <div className="site-info">
           Made with DraftBox ·{" "}
           <a href="/rss.xml" style={{ fontStyle: "normal" }}>
             RSS <img style={{ height: "12px" }} src={rssLogo} alt="" />
           </a>{" "}
-        </div>{" "}
+        </div>{" "} */}
       </footer>
+      <hr />
+      <div className="credits">
+        <a href="">Published with DraftBox</a>
+      </div>
     </div>
   );
 };
